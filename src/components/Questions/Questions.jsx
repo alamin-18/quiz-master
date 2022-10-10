acc7d6bd34387fa8a42acb6e6aaa27010ee6a60e
Md Al Amin <fpalamin6@gmail.com>
2022-10-11 03:06:19 +0600

    correct answer added

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import Options from '../Options/Options';
 
 const Questions = ({quiz}) => {
-    const {question,options,id} =quiz
+    const {question,options,id,correctAnswer} =quiz
 
-
-    console.log(quiz);
-    const quizHandle = ()=>{
-        console.log("kaj kortichi");
+// console.log(options);
+    // console.log(quiz);
+    const quizHandle = (option)=>{
+        if(option === correctAnswer){
+            console.log("tik hoyche re");
+        }
+        else{
+            console.log("vul hoyche re")
+        }
     }
     return (
         <div className=''>
             <h1 className='text-3xl font-semibold mt-6 mb-6'> {question}</h1>
             <div className='grid lg:grid-cols-2 gap-6 '> 
                 {
-                    quiz.map(option =><Options quizHandle={quizHandle} option={option}></Options>)
+                    options.map((option,index) =><div onClick={()=>quizHandle(option)} className='border border-violet-600 text-xl py-6 px-2 flex gap-4 justify-center items-center'>
+                    <input type="radio" id={id} name='option' />
+                    <label htmlFor={id}>{option}</label>
+                </div>)
                 }
                 {/* <div className='border border-violet-600 text-xl py-6 px-2 flex gap-4 justify-center items-center'>
             <input type="radio" />
