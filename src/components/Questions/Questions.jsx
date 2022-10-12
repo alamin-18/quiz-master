f0ecf6fb0359797bc8b4e5a05ba1aed0b88f6682
Md Al Amin <fpalamin6@gmail.com>
2022-10-12 14:36:02 +0600

    toast modify

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,49 +1,13 @@
 import React from 'react';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer,toast } from 'react-toastify';
+
  
 
-const Questions = ({quiz}) => {
+const Questions = ({quiz,Wronganswer,seeanswer,correctanswer}) => {
     const {question,options,correctAnswer} =quiz
 
 // console.log(options);
 // const notify = () => toast("Wow so easy!");
-const correctanswer = () => {
-    toast.success('Wow your is correct!', {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        });
-}
-const seeanswer = () => {
-    toast.success(correctAnswer, {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        });
-}
-const Wronganswer = () =>{
-    toast.warn('Your answer is wrong!', {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        });
-}
+
     
     const quizHandle = (option)=>{
         if(option === correctAnswer){
@@ -59,7 +23,7 @@ const Wronganswer = () =>{
         <div>
             <div>
             <h1 className='text-3xl font-semibold mt-6 mb-6'> {question}</h1>
-            <button onClick={seeanswer}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"       fill="currentColor" className="w-6 h-6">
+            <button onClick={()=>seeanswer(correctAnswer)}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"       fill="currentColor" className="w-6 h-6">
                         <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
                         <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
                         </svg>
@@ -73,7 +37,7 @@ const Wronganswer = () =>{
                 }
             </div>
             </div>
-                <ToastContainer></ToastContainer>
+                
         </div>
     );
 };
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Questions from '../Questions/Questions';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer,toast } from 'react-toastify';
 
 const Quiz = () => {
     const quizsload = useLoaderData()
     const quizs = quizsload.data.questions
+
+
+    // toast
+    const correctanswer = () => {
+        toast.success('Wow your is correct!', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+    }
+    const seeanswer = (correctAnswer) => {
+        toast.success(correctAnswer, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+    }
+    const Wronganswer = () =>{
+        toast.warn('Your answer is wrong!', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+    }
+    // toast
+
     
     return (
         <div className='mt-5'>
@@ -14,10 +57,16 @@ const Quiz = () => {
             <div className='md:w-3/4 lg:w-1/3 mx-auto drop-shadow-2xl'>
             {
                 quizs.map(quiz =>
-                    <Questions key={quiz.id} quiz={quiz}></Questions>
+                    <Questions
+                     key={quiz.id} quiz={quiz}
+                     Wronganswer={Wronganswer}
+                     seeanswer={seeanswer}
+                     correctanswer={correctanswer}
+                     ></Questions>
                 )
             }
             </div>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
